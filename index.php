9c964bb573a495778905af0fe200e0435e7a7520
Yeferson Camilo Zaque <zaquecamilo@gmail.com>
2021-11-06 19:38:20 -0500

    Añadida información básica al index

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php require_once __DIR__ . '/templates/header.php'; ?>
 
-<main>
-    <!-- Struct -->
+<main class="container">
+    <h1>Avances Desarrollo Backend</h1>
+    <div class="text-center">
+        <p><strong>Estudiante:</strong> Camilo Zaque</p>
+        <p><strong>Fecha:</strong> 06 de Noviembre del 2021</p>
+        <p><strong>Profesor:</strong> Francisco Hilarión</p>
+    </div>
 </main>
 
 <?php require_once __DIR__ . '/templates/footer.php'; ?>
